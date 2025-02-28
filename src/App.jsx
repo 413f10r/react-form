c9e7162ba8ff2336b29a7d8940e16a62387027e7
413f10r <ale-fioretti@live.it>
2025-02-28 20:02:08 +0100

    remove components - adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,64 @@
 import { useState } from 'react'
-import Header from './components/Header'
+
+import Articles from './data/articles'
+
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [articles, setArticles] = useState(Articles)
+  const [newArticles, setNewArticles] = useState('');
 
+  {/*gestione imput */ }
+  const handleNewArticles = (e) => {
+    {/* aggiorno lo stato iniziale della variabile new Articles */ }
+    setNewArticles(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setArticles(() => [...articles, { title: newArticles.trim() }])
+    {/* pulizia imput */ }
+    setNewArticles('')
+  }
   return (
     <>
-    < Header/>
-    // </>
-    // </>
+      <div className="container ">
+
+        <h1>Articles List</h1>
+        {
+
+          articles.length > 0 ? (
+            <ul>
+              {
+                articles.map((Element, index) => {
+                  return (
+                    <li key={index}>
+                      {Element.title}
+                    </li>
+                  )
+                })
+              }
+
+            </ul>) : <h3>Non ci sono articoli</h3>
+        }
+
+
+        <h3>Add Articles</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text"
+            value={newArticles}
+            onChange={handleNewArticles}
+            placeholder="Add new Article"
+          />
+
+          <button className="btn btn-primary">Add new Article</button>
+        </form>
+
+
+
+      </div>
+    </>
   )
 }
 
